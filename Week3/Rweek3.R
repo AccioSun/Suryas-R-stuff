51ff1e1210668273172aeaace8d6207f1a69df01
Surya Dhulipala <46639270+AccioSun@users.noreply.github.com>
2019-01-28 00:34:51 -0500

    Rweek3R changes
    Added  the results.df and colmeans to  rweek3.r

---
+++ Week3/Rweek3.R
@@ -0,0 +1,6 @@
+Name <- c("Jeb", "Donald", "Ted","Marco", "Carly", "Hillary", "Berine")
+ABCresults <- c(4, 62, 51, 21, 2, 14, 15)
+CBSresults <- c(12, 75, 43, 19, 1, 21, 19)
+results <- cbind(Name, ABCresults, CBSresults)
+results.df <- data.frame(Name, ABCresults, CBSresults)
+colMeans(results.df[,2:3])
