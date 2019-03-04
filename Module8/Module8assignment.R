80387e8f94242413eaca6f096cd9a0ed0df7e08b
Surya Dhulipala <46639270+AccioSun@users.noreply.github.com>
2019-03-04 02:19:52 -0500

    finished file

--- Module8/Module8assignment.R
+++ Module8/Module8assignment.R
@@ -1,1 +1,11 @@
-X <- read.table("Assignment_6_dataset.txt", header = TRUE)
+getwd()
+#add the table from the directory
+x <- read.table("Assignment 6 dataset.txt", header = TRUE, sep=",")
+#install pryr
+install.packages("pryr")
+library(pryr)
+StudentAverage <- ddply(x, "Sex", transform, Grade.Average=mean(Grade))
+write.table(StudentAverage, "Sorted Average", sep=",")
+#this creates a table, y, with the new information.
+iNames <- subset(x, grepl("i", x$Name, ignore.case=T))
+write.table(iNames, "DataSubsetI", sep=",")
