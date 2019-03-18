5193c20993cc24c390152b793e01f7104bf988fd
Surya Dhulipala <46639270+AccioSun@users.noreply.github.com>
2019-03-18 03:07:15 -0400

    assignment

---
+++ module10/DESCRIPTIONmockup.R
@@ -0,0 +1,10 @@
+Package: SDhulipala
+Title: Model of disease spread rate
+Version: 0.0.0.9000
+Authors@R: "Surya Dhulipala <sdhulipala@health.usf.edu> [aut, cre]"
+Description: Functions for predicting the change in spread rate of diseases
+    as function of the density of vaccinated and unvaccinated individuals
+    in the area
+Depends: R (>= 3.1.2)
+License: CC0
+LazyData: true
